5fef02340364dd9808c10a6b3226ec7f6204a680
Kevin Reyes <kerpa9@gmail.com>
2024-09-11 17:11:01 -0500

    invert this roles into defender and attacker

--- src/battle.service.ts
+++ src/battle.service.ts
@@ -4,5 +4,21 @@ import { LoggerService } from "./logger.service";
 export class BattleServices {
   constructor(private logger: LoggerService) {}
 
-  battle(attacker: Character, defender: Character) {}
+  battle(attacker: Character, defender: Character) {
+    while (attacker.isAlive() && defender.isAlive()) {
+      this.logger.log(`${attacker.name} ataca ${defender.name}`);
+      attacker.attack(defender);
+
+      if (!defender.isAlive()) {
+        this.logger.log(`${defender.name} ha muerto`);
+        break;
+      } else {
+        this.logger.log(
+          `${defender.name} ha ${defender.health} puntos de vida`
+        );
+      }
+
+      [attacker, defender] = [defender, attacker];
+    }
+  }
 }
